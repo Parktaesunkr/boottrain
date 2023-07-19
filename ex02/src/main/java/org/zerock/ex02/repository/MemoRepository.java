cf34d9ccb47209233df112256dc08e0965e2945d
Parktaesunkr <spcsm5452@naver.com>
2023-07-19 16:37:37 +0900

    추가 테스트
    지정 범위 테스트

--- ex02/src/main/java/org/zerock/ex02/repository/MemoRepository.java
+++ ex02/src/main/java/org/zerock/ex02/repository/MemoRepository.java
@@ -1,8 +1,19 @@
 package org.zerock.ex02.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.zerock.ex02.entity.Memo;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to); // 특정 구간의 객체를 구하고 역순으로 정렬
+
+    Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
+
+    void deleteMemoByMnoLessThan(Long num);
+
 
 }
--- ex02/src/test/java/org/zerock/ex02/repository/MemoRepositoryTests.java
+++ ex02/src/test/java/org/zerock/ex02/repository/MemoRepositoryTests.java
@@ -7,9 +7,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex02.entity.Memo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -90,5 +92,28 @@ public class MemoRepositoryTests {
         result.get().forEach(memo->{
             System.out.println(memo);
         });
+    }
+
+    @Test
+    public void testQueryMethods(){
+        List<Memo> list = memoRepository
+                .findByMnoBetweenOrderByMnoDesc(70L,80L);
+        for(Memo memo : list){
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testQueryMethodWithPageable(){
+        Pageable pageable = PageRequest.of(0,10,Sort.by("mno").descending());
+        Page<Memo> result  = memoRepository.findByMnoBetween(10L,50L, pageable);
+        result.get().forEach(memo-> System.out.println(memo));
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteQueryMethods(){
+        memoRepository.deleteMemoByMnoLessThan(10L);
     }
 }
