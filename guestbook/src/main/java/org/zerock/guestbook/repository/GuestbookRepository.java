84ce743ec345bf9b12b0ac932411a4604770ce82
Parktaesunkr <spcsm5452@naver.com>
2023-07-21 16:02:34 +0900

    guestbook 설정중

---
+++ guestbook/src/main/java/org/zerock/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,8 @@
+package org.zerock.guestbook.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.guestbook.entity.Guestbook;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+
+}
