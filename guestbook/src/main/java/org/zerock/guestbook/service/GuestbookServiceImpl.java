88971a0bcd9d06af03485ab29ac64817fd5794b6
Parktaesunkr <spcsm5452@naver.com>
2023-07-25 16:56:45 +0900

    게시글 페이징 테스트

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -3,10 +3,17 @@ package org.zerock.guestbook.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 import org.zerock.guestbook.repository.GuestbookRepository;
+
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -30,4 +37,17 @@ public class GuestbookServiceImpl implements GuestbookService{
 
         return entity.getGno();
     }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+        Page<Guestbook> result = repository.findAll(pageable);
+        Function<Guestbook, GuestbookDTO> fn = (entity ->
+            entityToDto(entity));
+
+
+        return new PageResultDTO<>(result, fn);
+    }
+
 }
--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 
 @SpringBootTest
@@ -23,4 +25,25 @@ public class GuestbookServiceTests {
 
        System.out.println(service.register(guestbookDTO));
    }
+
+   @Test
+   public void testList(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+       System.out.println("PREV: "+resultDTO.isPrev());
+       System.out.println("NEXT: "+resultDTO.isNext());
+       System.out.println("TOTAL: "+resultDTO.getTotalPage());
+       System.out.println("------------------------------------------");
+       for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+           System.out.println(guestbookDTO);
+       }
+       System.out.println("================================================="); // 화면에 출력될 페이지 번호
+       resultDTO.getPageList().forEach(i-> System.out.println(i));
+   }
+
 }
