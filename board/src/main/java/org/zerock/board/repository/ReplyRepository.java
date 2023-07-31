d8b174216b11f1b0eb37197ab0c2dfa16c2f0104
Parktaesunkr <spcsm5452@naver.com>
2023-07-31 16:22:50 +0900

    댓글 백엔드 테스트

--- board/src/main/java/org/zerock/board/repository/ReplyRepository.java
+++ board/src/main/java/org/zerock/board/repository/ReplyRepository.java
@@ -4,10 +4,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
 
+import java.util.List;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    // Board 삭제시 댓글 삭제
     @Modifying
     @Query("delete from Reply r where r.board.bno =:bno")
-    void deleteByBno(@Param("bno") Long bno);
+    void deleteByBno(Long bno);
+
+    // 게시물로 댓글 목록 가져오기
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
