cbbef676f51c47e629d07a7f3c3fd7f1db6cc154
Parktaesunkr <spcsm5452@naver.com>
2023-08-02 14:48:25 +0900

    댓글 추가 기능 추가

--- board/src/main/java/org/zerock/board/controller/ReplyController.java
+++ board/src/main/java/org/zerock/board/controller/ReplyController.java
@@ -6,10 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.board.dto.ReplyDTO;
 import org.zerock.board.service.ReplyService;
 
@@ -28,5 +25,12 @@ public class ReplyController {
     public ResponseEntity<List<ReplyDTO>> getListByBoard(@PathVariable("bno") Long bno){
         log.info("bno: "+bno);
         return new ResponseEntity<>(replyService.getList(bno), HttpStatus.OK);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> register(@RequestBody ReplyDTO replyDTO){
+        log.info(replyDTO);
+        Long rno = replyService.register(replyDTO);
+        return new ResponseEntity<>(rno,HttpStatus.OK);
     }
 }
