c1574b6b69494815a2ef7ab133cd6b6060598c70
Parktaesunkr <spcsm5452@naver.com>
2023-08-03 13:55:05 +0900

    댓글 수정 및 삭제

--- board/src/main/java/org/zerock/board/controller/ReplyController.java
+++ board/src/main/java/org/zerock/board/controller/ReplyController.java
@@ -33,4 +33,22 @@ public class ReplyController {
         Long rno = replyService.register(replyDTO);
         return new ResponseEntity<>(rno,HttpStatus.OK);
     }
+
+
+    @DeleteMapping("/{rno}")
+    public ResponseEntity<String> remove(@PathVariable ("rno") Long rno){
+        log.info("Rno: " + rno);
+        replyService.remove(rno);
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @PutMapping("/{rno}")
+    public ResponseEntity<String> modify(@RequestBody ReplyDTO replyDTO){
+
+        log.info(replyDTO);
+
+        replyService.modify(replyDTO);
+
+        return  new ResponseEntity<>("success",HttpStatus.OK);
+    }
 }
