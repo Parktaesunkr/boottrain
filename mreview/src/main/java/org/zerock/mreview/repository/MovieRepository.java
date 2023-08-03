c035b801f55d348bee7959b4bf57e4ce314a7857
Parktaesunkr <spcsm5452@naver.com>
2023-08-03 16:08:13 +0900

    리뷰 리스트 테스트

--- mreview/src/main/java/org/zerock/mreview/repository/MovieRepository.java
+++ mreview/src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -1,8 +1,15 @@
 package org.zerock.mreview.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.zerock.mreview.entity.Movie;
 
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @Query("select m, avg(coalesce(r.grade,0)), count(distinct r) from Movie m" +
+            " left outer join Review r on r.movie = m group by m")
+    Page<Object[]>getListPage(Pageable pageable);
 }
--- mreview/src/test/java/org/zerock/mreview/repository/MovieRepositoryTests.java
+++ mreview/src/test/java/org/zerock/mreview/repository/MovieRepositoryTests.java
@@ -3,11 +3,16 @@ package org.zerock.mreview.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.MovieImage;
 
 import javax.transaction.Transactional;
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.IntStream;
 
@@ -47,5 +52,18 @@ public class MovieRepositoryTests {
             System.out.println("==========================================");
         });
 
+    }
+
+    @Test
+    public void testListPage(){
+
+        PageRequest pageRequest =
+                PageRequest.of(0,10, Sort.by(Sort.Direction.DESC, "mno"));
+
+        Page<Object[]> result = movieRepository.getListPage(pageRequest);
+
+        for (Object[] objects : result.getContent()){
+            System.out.println(Arrays.toString(objects));
+        }
     }
 }
